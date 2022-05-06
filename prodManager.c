58aa5752221451e29828f03a394e1373bcaad117
newwin01 <newwin0198@handong.ac.kr>
2022-05-06 12:15:18 +0900

    printRemain function created

--- prodManager.c
+++ prodManager.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "prodManager.h"
 
+
+
+
+int updateproduct(product *p){
+}
+
+int printRemain(product *p,int count){
+    for(int i=0;i<count;i++){
+        if(p[i].price==-1) continue;
+        printf("%s: %d개 남았습니다.\n",p[i].name,p[i].remain);
+    }
+}
+
+
 int main(){
     return 0;
 }
