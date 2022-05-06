e3bd41ee75f525f1b8b2af37db05bcd09d460f2d
newwin01 <newwin0198@handong.ac.kr>
2022-05-06 12:27:44 +0900

    updateproduct function created

--- prodManager.c
+++ prodManager.c
@@ -5,6 +5,36 @@
 
 
 int updateproduct(product *p){
+    int decision=0;
+    printf("이름 수정: ");
+    printf("수정하시려면 1번을 눌러주세요");
+    scanf("%d",&decision);
+    if(decision==1){
+        printf("이름 입력: ");
+        gets(p->name);
+    }
+    printf("정보 수정: ");
+    printf("수정하시려면 1번을 눌러주세요");
+    scanf("%d",&decision);
+    if(decision==1){
+        printf("정보 입력: ");
+        gets(p->info);
+    }
+    printf("가격 수정: ");
+    printf("수정하시려면 1번을 눌러주세요");
+    scanf("%d",&decision);
+    if(decision==1){
+        printf("가격 입력: ");
+        scanf("%d",p->price);
+    }
+    printf("수량 수정: ");
+    printf("수정하시려면 1번을 눌러주세요");
+    scanf("%d",&decision);
+    if(decision==1){
+        printf("수량 입력: ");
+        scanf("%d",p->remain);
+    }
+
 }
 
 int printRemain(product *p,int count){
@@ -13,6 +43,8 @@ int printRemain(product *p,int count){
         printf("%s: %d개 남았습니다.\n",p[i].name,p[i].remain);
     }
 }
+
+
 
 
 int main(){
